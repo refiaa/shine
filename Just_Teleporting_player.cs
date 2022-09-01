049d57f984c5596c502c82f8e792a21647e26c3a
Refia <112306763+refiaa@users.noreply.github.com>
2022-09-01 18:54:01 +0900

    123

--- Just_Teleporting_player.cs
+++ Just_Teleporting_player.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMove : MonoBehaviour
+public class MovePlayer : MonoBehaviour
 {
-    public Transform TargetPosition;
-
-    private void Start()
-    {
-        
-    }
+    public Animation Anim;
+    float MoveSpeed = 2.5f;
+    float TurnSpeed = 100f;
 
     private void Update()
     {
-        
+        float V = Input.GetAxis("Vertical");
+        float H = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * V * MoveSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up * H * TurnSpeed * Time.deltaTime);
+
+        if (V == 0f) Anim.CrossFade("Idle", 0.25f);
+        else Anim.CrossFade("Walk", 0.25f);
     }
 }
--- Just_Teleporting_portal.cs
+++ Just_Teleporting_portal.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMove : MonoBehaviour
+public class Portal : MonoBehaviour
 {
-    public Transform TargetPosition;
+    public Transform TranslatePosition;
 
-    private void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
+        if(other.gameObject.tag == "Player")
+        {
+            Transform ParentTransform = other.transform;
+            
+            while (true)
+            {
+                if (ParentTransform.parent == null)
+                    break;
+                else
+                    ParentTransform = ParentTransform.parent;
 
-    private void Update()
-    {
-        
+            }
+
+            ParentTransform.position = TranslatePosition.position;
+            ParentTransform.rotation = TranslatePosition.rotation;
+        }
     }
 }
