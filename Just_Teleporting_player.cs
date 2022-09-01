5987b005c4a0e3adfe198d4de021db567a02329e
Refia <112306763+refiaa@users.noreply.github.com>
2022-09-01 18:43:19 +0900

    ak22

---
+++ Just_Teleporting_player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Transform TargetPosition;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
